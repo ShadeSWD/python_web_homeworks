04769294907a4fad1c1df55b5ff565d53ed6da0d
ShadeSWD <seawolfy60@gmail.com>
2023-09-02 16:26:22 +0300

    add views

--- hw2/catalog/views.py
+++ hw2/catalog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, "catalog/home.html")
+
+
+def contacts(request):
+    if request.method == 'POST':
+        visitor = dict()
+        visitor['name'] = request.POST.get('name', None)
+        visitor['phone'] = request.POST.get('phone', None)
+        visitor['message'] = request.POST.get('message', None)
+        print(visitor)
+    return render(request, "catalog/contacts.html")
