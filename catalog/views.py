0ee488e84c4d97a01dc018c4251dab76d1bbddb9
ShadeSWD <seawolfy60@gmail.com>
2023-09-08 13:29:49 +0300

    add contacts

--- catalog/views.py
+++ catalog/views.py
@@ -8,10 +8,15 @@ def home(request):
 
 
 def contacts(request):
+    context = {
+        'title': 'Contacts',
+        'contact_info': Contacts.objects.first(),
+    }
+
     if request.method == 'POST':
         visitor = dict()
         visitor['name'] = request.POST.get('name', None)
         visitor['phone'] = request.POST.get('phone', None)
         visitor['message'] = request.POST.get('message', None)
         print(visitor)
-    return render(request, "catalog/contacts.html")
+    return render(request, "catalog/contacts.html", context=context)
