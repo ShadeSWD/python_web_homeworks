1084e228300c49be1ecbfa178066988edbc745ef
ShadeSWD <seawolfy60@gmail.com>
2023-09-08 19:41:11 +0300

    add str to contacts

--- catalog/models.py
+++ catalog/models.py
@@ -29,3 +29,6 @@ class Contacts(models.Model):
     country = models.CharField(max_length=150, verbose_name='country')
     itn = models.CharField(max_length=30, verbose_name='ITN')
     address = models.CharField(max_length=200, verbose_name='address')
+
+    def __str__(self):
+        return f"Contacts(pk={self.pk}, country={self.country!r})"
