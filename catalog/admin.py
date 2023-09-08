2ccc4f69930db571fde4fdf4c008d9e22016473f
ShadeSWD <seawolfy60@gmail.com>
2023-09-08 20:00:07 +0300

    search in category

--- catalog/admin.py
+++ catalog/admin.py
@@ -5,6 +5,7 @@ from catalog.models import Category, Product, Contacts
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('pk', 'category_name')
+    search_fields = ('category_name', )
 
 
 @admin.register(Product)
