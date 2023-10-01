0b870296d63c2ef70fd576d7dfcb2b0983f1c05b
ShadeSWD <seawolfy60@gmail.com>
2023-10-01 23:12:48 +0300

    fix version creation

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,7 @@
-from django.forms import formset_factory
+from django.forms import inlineformset_factory
 from catalog.forms import ProductForm, VersionForm
 from catalog.models import Product, Version
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.shortcuts import get_object_or_404
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
@@ -37,13 +37,26 @@ class ProductUpdateView(UpdateView):
     context_object_name = 'product'
     success_url = reverse_lazy("catalog:list")
 
-    def get_object(self, queryset=None):
-        pk = self.kwargs.get('pk')
-        return get_object_or_404(Product, pk=pk)
+    def get_success_url(self):
+        return reverse('catalog:update', args=[self.kwargs.get('pk')])
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        VersionFormset = inlineformset_factory(Product, Version, form=VersionForm, extra=1)
+        if self.request.method == 'POST':
+            formset = VersionFormset(self.request.POST, instance=self.object)
+        else:
+            formset = VersionFormset(instance=self.object)
+        context_data['version_formset'] = formset
+        return context_data
 
     def form_valid(self, form):
-        new_product = form.save()
-        new_product.save()
+        context_data = self.get_context_data()
+        formset = context_data['version_formset']
+        self.object = form.save()
+        if formset.is_valid():
+            formset.instance = self.object
+            formset.save()
         return super().form_valid(form)
 
 
